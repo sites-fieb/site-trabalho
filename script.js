a7f09b1bc7fa60389579d344710c5e98be4d4a75
sites-fieb <142956254+sites-fieb@users.noreply.github.com>
2023-08-22 23:13:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+var botao = document.getElementById("meuBotao");
+
+botao.addEventListener("click", function() {
+    window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSf_5D0vtIZ4FIX1SKH9bRcadfSnVIhoGFXJo3jtjQ92z99QZw/viewform?vc=0&c=0&w=1&flr=0&fbzx=2063180062390570885";
+});
